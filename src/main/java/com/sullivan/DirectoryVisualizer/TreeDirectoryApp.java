b56d99190be651637afe6b91edc8ef0f313b8cea
Hayden Sullivan <shsullivan2@gmail.com>
2025-08-21 21:44:50 -0400

    Added application option list (CLI GUI)

--- src/main/java/com/sullivan/DirectoryVisualizer/TreeDirectoryApp.java
+++ src/main/java/com/sullivan/DirectoryVisualizer/TreeDirectoryApp.java
@@ -1,4 +1,19 @@
 package com.sullivan.DirectoryVisualizer;
 
+import java.util.Scanner;
+
 public class TreeDirectoryApp {
+    public static void main(String[] args) {
+        boolean run = true;
+        Scanner sc = new Scanner(System.in);
+
+        while (run) {
+            System.out.println("\nWelcome to Directory Visualizer");
+            System.out.println("1. Create Directory Tree Visualizer");
+            System.out.println("2. Exit Application");
+            System.out.println("Select from the options above: ");
+            String choice = sc.nextLine();
+            run = false;
+        }
+    }
 }
